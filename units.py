2b43a6e5fe7f7c5fc45fc2d5db16504a13acd6e7
Влад Трушков <trushkov@yandexlyceum.ru>
2021-07-03 15:15:05 +0500

    Инициализация PyGame. Добавлена FPS. Создан первый тестовый спрайт.

---
+++ units.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
