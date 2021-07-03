462d340bc94e0d35f744cbbe477a8f3a29258574
Влад Трушков <trushkov@yandexlyceum.ru>
2021-07-03 15:08:33 +0500

    Инициализация PyGame. Добавлена FPS. Создан первый тестовый спрайт.

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from settings import *
+from units import *
+
+
 def main():
-    print('10')
-    print('20')
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    clock = pygame.time.Clock()
+
+    x_pos = 0
+    v = 20  # пикселей в секунду
+    running = True
+
+    all_sprites = pygame.sprite.Group()
+    sprite = pygame.sprite.Sprite(all_sprites)
+    sprite.image = load_image(r"sprites\human\x_startpoint.png")
+    sprite.rect = sprite.image.get_rect()
+
+    while running:
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+        sprite.rect.x = 100
+        sprite.rect.y = 200
+        #screen.fill((0, 0, 0))
+        #pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), 200), 20)
+        #x_pos += v * clock.tick() / 1000  # v * t в секундах
+        all_sprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
